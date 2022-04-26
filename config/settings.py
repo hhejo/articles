b76f0eff3bab72a15c56f79713e7c4dd274d8c59
hhejo <litsmainjof@gmail.com>
2022-04-26 23:29:15 +0900

    SET settings.py

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,7 @@ SECRET_KEY = get_secret('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.pythonanywhere.com']
+ALLOWED_HOSTS = [*'', '.pythonanywhere.com']
 
 
 # Application definition
