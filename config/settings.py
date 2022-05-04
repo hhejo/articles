3741ef6cd5145dc502a0c40b6efbd3ee24820907
hhejo <litsmainjof@gmail.com>
2022-05-04 20:31:12 +0900

    set debug false

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,7 @@ SECRET_KEY = get_secret('SECRET_KEY')
 #### for Django Secret Key ####
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', '.pythonanywhere.com']
 
